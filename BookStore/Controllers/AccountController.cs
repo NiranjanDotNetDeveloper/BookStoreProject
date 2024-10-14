9554ae15539d9891b7bb3efbecb070414a9819a0
Niranjan <iniranjankumarsingh.gmail.com>
2024-10-15 01:03:15 +0530

    Added Account Pages

---
+++ BookStore/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonUI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonUI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class PersonController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
